3a0596babb81107be8096cbd764ba02deffae107
Isabelle Fernandes de Oliveira <isabelleferoliveira@gmail.com>
2024-10-15 17:18:55 -0300

    formatura com louvor!

--- web_dev_challenges/02_lists_tuple_dict/03_tuple_diversos/tuple_methods.py
+++ web_dev_challenges/02_lists_tuple_dict/03_tuple_diversos/tuple_methods.py
@@ -1,16 +1,24 @@
 def tuple_sum():
-    # TODO: Calcule a soma dos elementos da  tupla 'my_tuple'
     my_tuple = 1, 2, 3, 4, 5
-    pass
+    soma = 0
+
+    for i in range(len(my_tuple)):
+        soma += my_tuple[i]
+    
+    return(soma)
+
 
 def find_second_element():
     # TODO: Retorne o segundo elemento da tupla 'my_tuple'
     my_tuple = 1, 2, 3, 4, 5
-    pass
+    return(my_tuple[1])
+
 
 def find_out_typle_error():
     # TODO: Encontrar o erro no código abaixo
     # TODO: my_tuple não deverá ser modificada
     my_tuple = 1, 2, 3, 4, 5
-    typle.remove(1)
+    x = list(my_tuple)
+    x.remove(1)
+    x = tuple(x)
     return my_tuple
